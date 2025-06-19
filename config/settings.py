2a9a43be21f5e90bbd3a5b9edc12e644f6fe937f
Павло Гладкий <hladkyy.p@nltu.lviv.ua>
2025-06-19 15:09:35 +0300

    added api_key

--- config/settings.py
+++ config/settings.py
@@ -6,4 +6,4 @@ load_dotenv()
 TELEGRAM_API_ID = os.getenv('TELEGRAM_API_ID')
 TELEGRAM_API_HASH = os.getenv('TELEGRAM_API_HASH')
 TELEGRAM_PHONE = os.getenv('TELEGRAM_PHONE')
-
+API_KEY = os.getenv('API_KEY')
